6b472791c02e2c9a13761e8614f30ee5062efe5e
Badr_Annabi <badrannabi@gmail.com>
2024-06-14 00:01:33 +0000

    TASK 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- This SQL script creates a table users
 CREATE TABLE IF EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255),
+	email  VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	PRIMARY KEY(id));
