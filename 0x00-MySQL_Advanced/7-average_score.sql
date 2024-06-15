51ede555542db5f29adde4806e405751bb4b1447
Badr_Annabi <badrannabi@gmail.com>
2024-06-15 20:03:09 +0000

    change user_id params to input_user_id

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,16 +2,16 @@
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
 BEGIN
 	DECLARE avg_score DECIMAL(10, 2);
 	SELECT ROUND(AVG(score), 2) INTO avg_score
 	FROM corrections
-	WHERE user_id = user_id;
+	WHERE user_id = input_user_id;
 
 	UPDATE users
 	SET average_score = avg_score
-	WHERE id = user_id;
+	WHERE id = input_user_id;
 END$$
 
 DELIMITER ;
