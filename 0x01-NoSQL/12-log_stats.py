16052547f055e9b43c10b264a46feeb1a0ffd7e5
Badr_Annabi <badrannabi@gmail.com>
2024-06-18 14:22:12 -0700

    Fix some typo issues

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 def log_stats():
     """log stats func"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nfinx
+    logs_collection = client.logs.nginx
     total = logs_collection.count_documents({})
     get = logs_collection.count_documents({"method": "GET"})
     post = logs_collection.count_documents({"method": "POST"})
@@ -18,12 +18,12 @@ def log_stats():
             {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
-    print(f"\method GET: {get}")
-    print(f"\method POST: {post}")
-    print(f"\method PUT: {put}")
-    print(f"\method PATCH: {patch}")
-    print(f"\method DELETE: {delete}")
-    print(f"{path}status check")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
 
 
 if __name__ == "__main__":
