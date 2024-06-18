71fd3af9005beea08382a01234e48e374b63ae5f
Badr_Annabi <badrannabi@gmail.com>
2024-06-18 08:45:24 -0700

    Add multi

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,4 +10,5 @@ def update_topics(mongo_collection, name, topics):
             {"name": name},
             {
                 "set": {"topics": topics}
-            })
+            },
+            {multi: true})
