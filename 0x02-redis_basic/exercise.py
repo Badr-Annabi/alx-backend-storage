45d2a1eaf6f93855d5c254705c64e5b83036b85b
Badr_Annabi <badrannabi@gmail.com>
2024-06-19 23:17:08 +0000

    TASK 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Creating a Cache Class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache():
     def __init__(self):
@@ -19,3 +19,24 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Get sata from cache"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ Get a string from the cache """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """ Get an int from the cache  """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
