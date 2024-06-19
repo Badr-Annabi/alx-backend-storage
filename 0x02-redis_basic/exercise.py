69934e3879cea88b2446401e1ededc7078343b35
Badr_Annabi <badrannabi@gmail.com>
2024-06-19 23:27:58 +0000

    TASK 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -31,14 +31,8 @@ class Cache():
 
     def get_str(self, key: str) -> str:
         """ Get a string from the cache """
-        value = self._redis.get(key)
-        return value.decode('utf-8')
+        return self.get(key, fn=str)
 
     def get_int(self, key: str) -> int:
         """ Get an int from the cache  """
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode('utf-8'))
-        except Exception:
-            value = 0
-        return value
+        return self.get(key, fn=int)
