844ecc7ec7fc129484e546956b3d98e8c39e915e
Badr_Annabi <badrannabi@gmail.com>
2024-06-20 00:49:31 +0000

    TASK 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -35,7 +35,13 @@ def call_history(method: Callable) -> Callable:
 
 
 def replay(method: Callable) -> None:
-    """ Replays the history of a function  """
+    """
+    Replays the history of a function
+    Args:
+        method: The function to be decorated
+    Returns:
+        None
+    """
     name = method.__qualname__
     cache = redis.Redis()
     calls = cache.get(name).decode("utf-8")
@@ -43,10 +49,8 @@ def replay(method: Callable) -> None:
     inputs = cache.lrange(name + ":inputs", 0, -1)
     outputs = cache.lrange(name + ":outputs", 0, -1)
     for i, o in zip(inputs, outputs):
-        print(
-                "{}(*{}) -> {}".format(
-                    name, i.decode(
-                        'utf-8'), o.decode('utf-8')))
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
 
 
 class Cache():
